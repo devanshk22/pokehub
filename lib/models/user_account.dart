84fa47fdae4ae007bfb89576f934242d1dd46ef5
Devansh Koppar <devanshk22@gmail.com>
2021-12-24 20:34:50 +0530

    Fleshing out user class, creating empty subclasses

--- lib/models/user_account.dart
+++ lib/models/user_account.dart
@@ -1,5 +1,14 @@
+import 'package:pokehub/models/card.dart';
+import 'package:pokehub/models/card_collection.dart';
+import 'package:pokehub/models/deck.dart';
+
 class UserAccount {
   late final String uid;
+  String name = "";
+  String email = "";
+  List<Card> cardCollection = [];
+  List<Deck> userDecks = [];
+  List<CardCollection> userCollections = [];
 
   UserAccount({required this.uid});
 }
