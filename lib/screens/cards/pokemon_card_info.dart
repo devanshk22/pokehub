e8f5cafa5975c8da496898880ac8e3bd79929bb0
Devansh Koppar <devanshk22@gmail.com>
2022-02-28 18:27:01 +0800

    Adding price info, attacks and abilities to card info page

--- lib/screens/cards/pokemon_card_info.dart
+++ lib/screens/cards/pokemon_card_info.dart
@@ -31,6 +31,8 @@ class _PokemonCardInfoState extends State<PokemonCardInfo> {
         body: Align(
           alignment: Alignment.center,
           child: ListView(
+            padding: EdgeInsets.symmetric(
+                horizontal: SizeConfig.blockSizeVertical * 2),
             scrollDirection: Axis.vertical,
             shrinkWrap: true,
             children: [
@@ -239,10 +241,11 @@ class _PokemonCardInfoState extends State<PokemonCardInfo> {
                           Text(
                             "Resistance",
                             style: TextStyle(
-                                color: Colors.white,
-                                fontFamily: 'Blinker',
-                                fontSize: SizeConfig.blockSizeVertical * 3,
-                                fontWeight: FontWeight.bold),
+                              color: Colors.white,
+                              fontFamily: 'Blinker',
+                              fontSize: SizeConfig.blockSizeVertical * 3,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
                           Text(
                             widget.card.resistances.isEmpty
@@ -278,6 +281,7 @@ class _PokemonCardInfoState extends State<PokemonCardInfo> {
                         children: [
                           Text(
                             "Retreat Cost",
+                            textAlign: TextAlign.center,
                             style: TextStyle(
                                 color: Colors.white,
                                 fontFamily: 'Blinker',
@@ -300,10 +304,169 @@ class _PokemonCardInfoState extends State<PokemonCardInfo> {
                   ),
                 ],
               ),
+              SizedBox(
+                height: widget.card.abilities.isEmpty
+                    ? 0
+                    : SizeConfig.blockSizeVertical * 3,
+              ),
+              Text(
+                widget.card.abilities.isEmpty ? "" : "Abilities:",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontFamily: 'Blinker',
+                  fontSize: SizeConfig.blockSizeVertical * 4,
+                ),
+              ),
+              SizedBox(
+                height: widget.card.abilities.isEmpty
+                    ? 0
+                    : SizeConfig.blockSizeVertical * 3,
+              ),
+              ...getAbilities(widget.card),
+              SizedBox(
+                height: SizeConfig.blockSizeVertical * 3,
+              ),
+              Text(
+                widget.card.attacks.isEmpty ? "" : "Attacks:",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontFamily: 'Blinker',
+                  fontSize: SizeConfig.blockSizeVertical * 4,
+                ),
+              ),
+              SizedBox(
+                height: widget.card.attacks.isEmpty
+                    ? 0
+                    : SizeConfig.blockSizeVertical * 3,
+              ),
+              ...getAttacks(widget.card),
+              SizedBox(
+                height: widget.card.attacks.isEmpty
+                    ? 0
+                    : SizeConfig.blockSizeVertical * 3,
+              ),
+              Text(
+                "Price Data (Source: tcgplayer.com)",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontFamily: 'Blinker',
+                  fontSize: SizeConfig.blockSizeVertical * 4,
+                ),
+              ),
+              Text(
+                r"Price Range:  $" +
+                    widget.card.tcgPlayer["prices"]["unlimitedHolofoil"]["high"]
+                        .toString() +
+                    r" - $" +
+                    widget.card.tcgPlayer["prices"]["unlimitedHolofoil"]["low"]
+                        .toString(),
+                style: TextStyle(
+                  color: Colors.white,
+                  fontFamily: 'Blinker',
+                  fontSize: SizeConfig.blockSizeVertical * 3,
+                ),
+              ),
+              Text(
+                r"Market Price:  $" +
+                    widget
+                        .card.tcgPlayer["prices"]["unlimitedHolofoil"]["market"]
+                        .toString(),
+                style: TextStyle(
+                  color: Colors.white,
+                  fontFamily: 'Blinker',
+                  fontSize: SizeConfig.blockSizeVertical * 3,
+                ),
+              ),
+              SizedBox(
+                height: SizeConfig.blockSizeVertical * 3,
+              ),
             ],
           ),
         ),
       ),
     );
+  }
+
+  List<Widget> getAbilities(PokemonCard card) {
+    print(card.tcgPlayer["prices"]["unlimitedHolofoil"]);
+    List<Widget> abilities = [];
+    if (card.abilities.isEmpty) {
+      return [];
+    } else {
+      for (Ability ability in card.abilities) {
+        abilities.add(Text(
+          ability.name + ": ",
+          style: TextStyle(
+            color: Colors.white,
+            fontFamily: 'Blinker',
+            fontSize: SizeConfig.blockSizeVertical * 3,
+            fontWeight: FontWeight.bold,
+          ),
+        ));
+        abilities.add(Text(
+          ability.text,
+          style: TextStyle(
+            color: Colors.white,
+            fontFamily: 'Blinker',
+            fontSize: SizeConfig.blockSizeVertical * 3,
+          ),
+        ));
+        abilities.add(
+          SizedBox(
+            height: SizeConfig.blockSizeVertical * 2,
+          ),
+        );
+      }
+      return abilities;
+    }
+  }
+
+  List<Widget> getAttacks(PokemonCard card) {
+    List<Widget> attacks = [];
+    if (card.attacks.isEmpty) {
+      return [];
+    } else {
+      for (Attack attack in card.attacks) {
+        List<String> cost = attack.cost;
+        String symbol_cost = cost.map((e) => "[" + e[0] + "] ").join();
+        attacks.add(Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              symbol_cost + " " + attack.name,
+              style: TextStyle(
+                color: Colors.white,
+                fontFamily: 'Blinker',
+                fontSize: SizeConfig.blockSizeVertical * 3,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              attack.damage!,
+              style: TextStyle(
+                color: Colors.white,
+                fontFamily: 'Blinker',
+                fontSize: SizeConfig.blockSizeVertical * 3,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ));
+        attacks.add(Text(
+          attack.text,
+          style: TextStyle(
+            color: Colors.white,
+            fontFamily: 'Blinker',
+            fontSize: SizeConfig.blockSizeVertical * 3,
+          ),
+        ));
+        attacks.add(
+          SizedBox(
+            height: SizeConfig.blockSizeVertical * 2,
+          ),
+        );
+      }
+      return attacks;
+    }
   }
 }
